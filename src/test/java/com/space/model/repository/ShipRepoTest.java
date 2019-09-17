47a9049fdd860cb7c3a4dca54e01233973b09071
Roman <41580710+PunkGagarin@users.noreply.github.com>
2019-09-17 13:10:15 +0300

    origin commit

--- src/test/java/com/space/model/repository/ShipRepoTest.java
+++ src/test/java/com/space/model/repository/ShipRepoTest.java
@@ -1,9 +1,15 @@
 package com.space.model.repository;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 
 public class ShipRepoTest {
+
+    @Test
+    public void test(){
+
+    }
 }
