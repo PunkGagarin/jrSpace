4c71191d0c7b7a45161ee5ba99de8c4ef26a82a5
Roman <41580710+PunkGagarin@users.noreply.github.com>
2019-10-07 11:25:02 +0300

    SpecBuilder

---
+++ src/main/java/com/space/service/utils/ShipSpecificationBuilder.java
@@ -0,0 +1,61 @@
+package com.space.service.utils;
+
+import com.space.model.ShipEntity;
+import com.space.model.ShipType;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Date;
+
+public class ShipSpecificationBuilder {
+
+    public static Specification<ShipEntity> stringSpec(String attribute, String value) {
+        return (root, query, criteriaBuilder) ->
+                value == null ? null : criteriaBuilder.equal(root.get(attribute), value);
+    }
+
+
+    public static Specification<ShipEntity> shipTypeSpec(ShipType shipType) {
+        return (root, query, criteriaBuilder) ->
+                shipType == null ? null : criteriaBuilder.equal(root.get("shipType"), shipType);
+    }
+
+    public static Specification<ShipEntity> prodDateSpec(Long milis, boolean isAfter) {
+        if (milis == null)
+            return null;
+
+        Date date = new Date(milis);
+        return ((root, query, criteriaBuilder) ->
+                isAfter ? criteriaBuilder.greaterThanOrEqualTo(root.get("prodDate"), date) :
+                        criteriaBuilder.lessThanOrEqualTo(root.get("prodDate"), date)
+        );
+    }
+
+    public static Specification<ShipEntity> doubleSpec(String attribute, Double number, boolean isGreater) {
+        if (number == null)
+            return null;
+
+        return ((root, query, criteriaBuilder) ->
+                isGreater ? criteriaBuilder.greaterThanOrEqualTo(root.get(attribute), number) :
+                        criteriaBuilder.lessThanOrEqualTo(root.get(attribute), number)
+        );
+    }
+
+    public static Specification<ShipEntity> intSpec(String attribute, Integer number, boolean isGreater) {
+        if (number == null)
+            return null;
+
+        return ((root, query, criteriaBuilder) ->
+                isGreater ? criteriaBuilder.greaterThanOrEqualTo(root.get(attribute), number) :
+                        criteriaBuilder.lessThanOrEqualTo(root.get(attribute), number)
+        );
+    }
+
+    public static Specification<ShipEntity> isUsedSpec(Boolean value) {
+        if (value == null)
+            return null;
+
+        return (root, query, criteriaBuilder) ->
+                value ? criteriaBuilder.isTrue(root.get("isUsed")) : criteriaBuilder.isFalse(root.get("isUsed"));
+    }
+
+}
